164010fdadf1cced1133ff62b83c4348208e843e
andreygolubkow <andreygolubkow@gmail.com>
2017-06-20 23:21:55 +0700

    Добавил правки к интерфейсу

--- Model/Model/IElement.cs
+++ Model/Model/IElement.cs
@@ -8,16 +8,23 @@ using System.Numerics;
 
 namespace Model
 {
+    //TODO: XML Комментарии
     interface IElement
     {
+        //TODO: XML Комментарии
         ElementTypes Type { get; }
 
+        //TODO: XML Комментарии
         string Name { get; set; }
 
+        //TODO: XML Комментарии
         double Value { get; set; }
 
+        //TODO: XML Комментарии
         Complex CalculateZ(double value);
 
+        //TODO: XML Комментарии
+        //TODO: У тебя в реализации событие находится выше чем рассчет Z. Мб тут тоже стоит поднять ? 
         event EventHandler ValueChanged;
     }
 }
